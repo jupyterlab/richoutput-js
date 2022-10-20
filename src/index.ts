29f61673c71a15af8d529d670a251d0be63b3da2
Jason Grout <jason@jasongrout.org>
2022-10-20 13:31:27 +0100

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,72 @@
+import { IRenderMime } from '@jupyterlab/rendermime-interfaces';
+
+import { Widget } from '@lumino/widgets';
+
+/**
+ * The default mime type for the extension.
+ */
+const MIME_TYPE = 'application/vnd.jupyter.es6-rich-output';
+
+/**
+ * The class name added to the extension.
+ */
+const CLASS_NAME = 'mimerenderer-ES6 Rich Output';
+
+/**
+ * A widget for rendering ES6 Rich Output.
+ */
+export class OutputWidget extends Widget implements IRenderMime.IRenderer {
+  /**
+   * Construct a new output widget.
+   */
+  constructor(options: IRenderMime.IRendererOptions) {
+    super();
+    this._mimeType = options.mimeType;
+    this.addClass(CLASS_NAME);
+  }
+
+  /**
+   * Render ES6 Rich Output into this widget's node.
+   */
+  renderModel(model: IRenderMime.IMimeModel): Promise<void> {
+    const data = model.data[this._mimeType] as string;
+    this.node.textContent = data.slice(0, 16384);
+    return Promise.resolve();
+  }
+
+  private _mimeType: string;
+}
+
+/**
+ * A mime renderer factory for ES6 Rich Output data.
+ */
+export const rendererFactory: IRenderMime.IRendererFactory = {
+  safe: true,
+  mimeTypes: [MIME_TYPE],
+  createRenderer: (options) => new OutputWidget(options),
+};
+
+/**
+ * Extension definition.
+ */
+const extension: IRenderMime.IExtension = {
+  id: 'richoutput-js:plugin',
+  rendererFactory,
+  rank: 100,
+  dataType: 'string',
+  fileTypes: [
+    {
+      name: 'ES6 Rich Output',
+      mimeTypes: [MIME_TYPE],
+      extensions: ['.es6richoutput'],
+    },
+  ],
+  documentWidgetFactoryOptions: {
+    name: 'ES6 Rich Output',
+    primaryFileType: 'ES6 Rich Output',
+    fileTypes: ['ES6 Rich Output'],
+    defaultFor: ['ES6 Rich Output'],
+  },
+};
+
+export default extension;
